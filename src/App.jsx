2cedc630c92eb0a761f57672a816da948113a6ad
FernandoLorca <flocapinto@gmail.com>
2023-03-01 18:05:01 -0300

    photo implement in main components ready

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,30 @@
+import { useState, useEffect } from "react";
+
 import Nav from "./components/Nav";
 import Main from "./components/Main/Main";
 import Footer from "./components/Footer";
 
 function App() {
+  const [dataPhotos, setDataPhotos] = useState([]);
+
+  const getPhotos = async () => {
+    const res = await fetch("../fotos.json");
+    const data = await res.json();
+
+    setDataPhotos(data.photos);
+  };
+
+  useEffect(() => {
+    getPhotos();
+  }, []);
+
+  console.log(dataPhotos);
+
   return (
     <>
       <Nav />
 
-      <Main />
+      <Main dataPhotos={dataPhotos} />
 
       <Footer />
     </>
